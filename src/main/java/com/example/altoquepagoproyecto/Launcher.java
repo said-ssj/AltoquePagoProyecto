0e98c31302da2b529cee79222fe155e4a65da208
Said Astohuaman <alvaro2016az@gmail.com>
2026-05-26 20:21:04 -0500

    Initial commit

---
+++ src/main/java/com/example/altoquepagoproyecto/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.altoquepagoproyecto;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
