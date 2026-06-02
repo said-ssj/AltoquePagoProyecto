7a360611e191e78c725e10fd81a2f054d4326209
cristian <cdhenao@gmail.com>
2026-06-02 18:42:35 -0500

    Actualizado

--- src/main/java/com/TestProducto.java
+++ src/main/java/com/TestProducto.java
@@ -1,36 +1,21 @@
-package com.controlador;
+package com;
+
 import com.dao.ProductoDAO;
 import com.modelo.Producto;
+
 public class TestProducto {
     public static void main(String[] args) {
 
-        ProductoDAO dao =
-                new ProductoDAO();
+        ProductoDAO dao = new ProductoDAO();
 
-        Producto producto =
-                dao.buscarPorCodigo(
-                        "789456"
-                );
+        Producto producto = dao.buscarPorCodigo("789456");
 
         if(producto != null){
-
-            System.out.println(
-                    "Producto encontrado"
-            );
-
-            System.out.println(
-                    producto.getNombre()
-            );
-
-            System.out.println(
-                    producto.getPrecio()
-            );
-
+            System.out.println("Producto encontrado");
+            System.out.println(producto.getNombre());
+            System.out.println(producto.getPrecio());
         }else{
-
-            System.out.println(
-                    "Producto no encontrado"
-            );
+            System.out.println("Producto no encontrado");
         }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,15 @@ module com.example.altoquepagoproyecto {
     requires javafx.controls;
     requires javafx.fxml;
 
-    // El permiso MySQL
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    requires java.desktop;
     requires java.sql;
+
     opens com.controlador to javafx.fxml;
     exports com.controlador;
-    exports com.modelo;
 }
