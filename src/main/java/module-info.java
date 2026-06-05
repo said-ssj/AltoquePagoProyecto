a005fe5dd37b85fe1237eb62edac652f7340024b
Said Astohuaman <alvaro2016az@gmail.com>
2026-06-05 11:55:53 -0500

    feat: actualizacion de vista reportes y dependencias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module com.example.altoquepagoproyecto {
     // El permiso MySQL
     requires java.sql;
     requires org.slf4j;
+    requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome5;
     opens com.controlador to javafx.fxml;
     exports com.controlador;
     exports com.modelo;
