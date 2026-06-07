02da1cad3c9092445e108613d0b76293b8e66866
Said Astohuaman <alvaro2016az@gmail.com>
2026-06-06 21:49:26 -0500

    style: se renombro Style-Table.css a Style-Tables.css style: se agrego css en Style-Ventas.css, Style-Empleados.css, Style-Productos.css para el combobox de filtros refactor: se adapto la logica de combobox a ventas-view.fxml refactor: se adapto la logica de combobox a productos-view.fxml refactor: se adapto la logica de combobox a empleados-view.fxml refactor: se adapto la logia de combobox a ControladorEmpleados.java refactor: se adapto la logia de combobox a ControladorProductos.java refactor: se adapto la logia de combobox a ControladorVentas.java

--- src/main/java/com/controlador/ControladorProductos.java
+++ src/main/java/com/controlador/ControladorProductos.java
@@ -1,4 +1,26 @@
 package com.controlador;
 
-public class ControladorProductos {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorProductos implements Initializable {
+
+    @FXML
+    private ComboBox<String> cbFiltrosProductos;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        // CONFIGURACION DEL COMBOBOX
+        cbFiltrosProductos.getItems().addAll("A - Z ⬆", "Z - A ⬇", "Categoria -  ", "Categoria - ","Categoria - ","IDs");
+        cbFiltrosProductos.setOnAction(e -> {
+            String seleccion = cbFiltrosProductos.getValue();
+            System.out.println("Filtrando por: " + seleccion);
+        });
+
+    }
+
 }
